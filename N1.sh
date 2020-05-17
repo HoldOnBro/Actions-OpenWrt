13aa6c53c99173e2bdb7886f510e7670f9f3ca2e
HoldOnBro <357835338@qq.com>
2020-05-17 13:00:09 +0800

    Update N1.sh
    Former-commit-id: a9fb1508fc3e342e6c586a1d33de19003cb7b67d

--- N1.sh
+++ N1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's/192.168.1.1/192.168.123.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.20/g' package/base-files/files/bin/config_generate
