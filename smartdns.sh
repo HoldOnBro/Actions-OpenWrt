0bd0e52b3ec35b46b90f29c31df305679860731b
HoldOnBro <357835338@qq.com>
2020-05-18 23:53:37 +0800

    Update smartdns.sh
    change to CTCGFW version

--- smartdns.sh
+++ smartdns.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/smartdns package/smartdns
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns package/luci-app-smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
