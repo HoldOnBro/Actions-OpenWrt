b8a587ea4c9cc30f487ed34ceffb44a2bf5c62c9
HoldOnBro <357835338@qq.com>
2020-05-23 10:07:50 +0800

    Delete smartdns.sh

--- smartdns.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/smartdns package/smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns package/luci-app-smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-
-./scripts/feeds update -a
-./scripts/feeds install -a
