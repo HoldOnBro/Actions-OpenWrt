8577811e7285e3b3ec4d1db6b670ca7e04034955
HoldOnBro <357835338@qq.com>
2020-10-15 05:27:26 +0800

    Update autoVersion.sh
    Former-commit-id: a0f3d3e8390b1c99d79ef8e42ca0dd7d22501493

--- aarch64/autoVersion.sh
+++ aarch64/autoVersion.sh
@@ -4,7 +4,7 @@ OpVersion=$(echo "$(cat /workdir/openwrt/package/lean/default-settings/files/zzz
 
 echo $OpVersion
 
-sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"t$OpVersion\"/" /opt/openwrt/mk_beikeyun_opimg.sh
+sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_beikeyun_opimg.sh
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_l1pro_opimg.sh
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_n1_opimg.sh
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_s905x3_opimg.sh
