b71f90734f49a4980269959bae751638e59bbc74
HoldOnBro <357835338@qq.com>
2020-10-14 19:43:57 +0800

    Update autoVersion.sh
    Former-commit-id: ee2f95c5868a8877703dabdc85495b07533dae18

--- aarch64/autoVersion.sh
+++ aarch64/autoVersion.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-result=$(echo "$(cat /home/xyz/configs/openwrt/zzz-default-settings)" | grep -Po "DISTRIB_REVISION=\'\K[^\']*")
+OpVersion=$(echo "$(cat /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings)" | grep -Po "DISTRIB_REVISION=\'\K[^\']*")
 
-echo $result
+echo $OpVersion
 
-sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$result\"/" /home/xyz/configs/openwrt/mk_beikeyun_opimg.sh
-sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$result\"/" /home/xyz/configs/openwrt/mk_l1pro_opimg.sh
-sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$result\"/" /home/xyz/configs/openwrt/mk_n1_opimg.sh
-sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$result\"/" /home/xyz/configs/openwrt/mk_s905x3_opimg.sh
-sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=$result/" /home/xyz/configs/openwrt/n1-to-vplus.sh
+sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_beikeyun_opimg.sh
+sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_l1pro_opimg.sh
+sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_n1_opimg.sh
+sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_s905x3_opimg.sh
+sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=$OpVersion/" /opt/openwrt/n1-to-vplus.sh
