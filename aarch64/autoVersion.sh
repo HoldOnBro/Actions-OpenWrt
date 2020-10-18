fe851fd3039c8bc67fafa76428f733c4834138ae
HoldOnBro <357835338@qq.com>
2020-10-18 19:28:25 +0800

    update compile files

--- aarch64/autoVersion.sh
+++ aarch64/autoVersion.sh
@@ -8,4 +8,5 @@ sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_beikeyun_o
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_l1pro_opimg.sh
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_n1_opimg.sh
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_s905x3_opimg.sh
+sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_s905x3_opimg_sfe.sh
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=$OpVersion/" /opt/openwrt/n1-to-vplus.sh
