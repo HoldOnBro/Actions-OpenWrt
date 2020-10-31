931fad506946333c1ca62549ffac117e609a55a9
HoldOnBro <357835338@qq.com>
2020-10-31 12:00:43 +0800

    decouple

---
+++ aarch64/getImgs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/mk_openwrt_src_20201028.tar.gz
+#sudo tar xvzf *.tar.gz
+#sudo mkdir imgs
+sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_Aml-s9xxx_buster_5.9.2-flippy-47+.img.xz
+#sudo xz -d Armbian*Aml*\+\.img\.xz
+sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_Aml-s9xxx_buster_5.4.73-flippy-47+o.img.xz
+#sudo xz -d Armbian*Aml*\+o*
+sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_Beikeyun_buster_5.4.73-flippy-47+o.img.xz
+#sudo xz -d Armbian*Beike*
+sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_L1-Pro_buster_5.4.73-flippy-47+o.img.xz
+#sudo xz -d Armbian*L1*
