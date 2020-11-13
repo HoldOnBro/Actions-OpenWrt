f6fed8bc6898746830535f4fe0b84aa52990a5bd
HoldOnBro <357835338@qq.com>
2020-11-14 07:49:22 +0800

    readd luci-app-dnsforwarder

--- diy-part2.sh
+++ diy-part2.sh
@@ -100,6 +100,6 @@ rm package/lean/coremark/coremark.sh
 cp $GITHUB_WORKSPACE/general/coremark.sh package/lean/coremark/
 
 #temp fix for dnsforwarder
-sed -i "s/PKG_SOURCE_URL:=.*/PKG_SOURCE_URL:=https:\/\/github\.com\/HoldOnBro\/dnsforwarder\.git/" package/lean/dnsforwarder/Makefile
-sed -i "s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=031c51fd187b18ac12fe8612f4cf4117a45a4fca/" package/lean/dnsforwarder/Makefile
-sed -i "s/\ \ URL:=.*/\ \ URL:=https:\/\/github\.com\/HoldOnBro\/dnsforwarder/" package/lean/dnsforwarder/Makefile
+sed -i "s/PKG_SOURCE_URL:=.*/PKG_SOURCE_URL:=https:\/\/github\.com\/1715173329\/dnsforwarder\.git/" package/lean/dnsforwarder/Makefile
+sed -i "s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=693b554e59479c2867c74f0bb5e26290b93747c5/" package/lean/dnsforwarder/Makefile
+sed -i "s/\ \ URL:=.*/\ \ URL:=https:\/\/github\.com\/1715173329\/dnsforwarder/" package/lean/dnsforwarder/Makefile
