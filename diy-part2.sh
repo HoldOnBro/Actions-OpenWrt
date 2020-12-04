35d8ea79fde8821526ee14d6c7f57ce47e15116b
HoldOnBro <357835338@qq.com>
2020-12-04 08:20:12 +0800

    add luci-app-autoipsetadder
    Former-commit-id: c5a00f066277a7e8227ecdba7cd68076acecb245

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,7 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git package/luci-app-autoipsetadder
 
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 cp -r package/OpenClash/luci-app-openclash package/
