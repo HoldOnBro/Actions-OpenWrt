5f9779cd0434237b7d9dd7c16ba57a847a6dd7c8
HoldOnBro <357835338@qq.com>
2020-12-15 08:14:43 +0800

    add luci-app-ttnode for arm devices

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,6 +19,7 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
