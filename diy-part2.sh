3f9dc1ed6fde8ee0a9686c141bb9a68db09d5053
HoldOnBro <357835338@qq.com>
2021-01-24 21:29:16 +0800

    routine update
    Former-commit-id: f6b73d8f26de48710d83f198015d9d5443e51a20

--- diy-part2.sh
+++ diy-part2.sh
@@ -46,7 +46,7 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/lienol/luci-app-
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
 #svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray package/xray
-
+svn co https://github.com/fw876/helloworld/trunk/xray /home/xyz/lede/package/xray
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-gost package/luci-app-gost
 svn co https://github.com/kenzok8/openwrt-packages/trunk/gost package/gost
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos package/luci-app-eqos
