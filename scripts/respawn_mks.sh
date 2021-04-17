2e74a8d7f65ade96f8f37acde27009f1f4e94aad
HoldOnBro <357835338@qq.com>
2021-04-17 10:05:54 +0800

    clean

--- scripts/respawn_mks.sh
+++ scripts/respawn_mks.sh
@@ -8,11 +8,10 @@ find ./*_plus.sh ./*_fol.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_T
 find ./*_plus.sh ./*_fol.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/F-openwrt/g' {}
 echo"mk_files respawned."
 
-
+cd ..
 #diff -uN openwrt/files/update-amlogic-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-amlogic-openwrt.sh > update-amlogic-openwrt.sh.patch
 #diff -uN openwrt/files/update-amlogic-openwrt-old.sh mk_openwrt_src_20210401/openwrt/files/update-amlogic-openwrt-old.sh > update-amlogic-openwrt-old.sh.patch
 #diff -uN openwrt/files/update-beikeyun-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-beikeyun-openwrt.sh > update-beikeyun-openwrt.sh.patch
-cd ..
 patch -p1 < update-amlogic-openwrt-old.sh.patch
 patch -p1 < update-amlogic-openwrt.sh.patch
 patch -p1 < update-beikeyun-openwrt.sh.patch
