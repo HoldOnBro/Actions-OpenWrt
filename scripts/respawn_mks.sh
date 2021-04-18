a517ffacc4ca09a22d16d8a8ef939c3e90dce08b
HoldOnBro <357835338@qq.com>
2021-04-18 09:54:59 +0800

    update script and rm luci-app-oaf for ARMv8 devices

--- scripts/respawn_mks.sh
+++ scripts/respawn_mks.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 cd openwrt
+sed -i '2,10 s/\(#\)\(.*\)/\2/' make.env
+OLD=$(grep \+o\" make.env)
+NEW=$(grep \+\" make.env)
+#echo $OLD
+#echo $NEW
 cp make.env makeplus.env
+sed -i "s/$NEW/#$NEW/" make.env
+sed -i "s/$OLD/#$OLD/" makeplus.env
+
 for F in *.sh ; do cp $F ${F%.sh}_plus.sh && cp $F ${F%.sh}_fol.sh;done
 find ./* -maxdepth 1 -path "*_plus.sh" | xargs -i sed -i 's/make\.env/makeplus\.env/g' {}
 find ./*_plus.sh ./*_fol.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"F-openwrt/g' {}
 find ./*_plus.sh ./*_fol.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/F-openwrt/g' {}
-echo"mk_files respawned."
+echo "mk_files respawned."
 
 cd ..
 #diff -uN openwrt/files/update-amlogic-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-amlogic-openwrt.sh > update-amlogic-openwrt.sh.patch
 #diff -uN openwrt/files/update-amlogic-openwrt-old.sh mk_openwrt_src_20210401/openwrt/files/update-amlogic-openwrt-old.sh > update-amlogic-openwrt-old.sh.patch
 #diff -uN openwrt/files/update-beikeyun-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-beikeyun-openwrt.sh > update-beikeyun-openwrt.sh.patch
+#diff -uN openwrt/files/update-vplus-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-vplus-openwrt.sh  > update-vplus-openwrt.sh.patch
+#diff -uN openwrt/files/update-l1pro-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-l1pro-openwrt.sh  > update-l1pro-openwrt.sh.patch
 patch -p1 < update-amlogic-openwrt-old.sh.patch
 patch -p1 < update-amlogic-openwrt.sh.patch
 patch -p1 < update-beikeyun-openwrt.sh.patch
-echo"patching done."
+patch -p1 < update-vplus-openwrt.sh.patch
+patch -p1 < update-l1pro-openwrt.sh.patch
+echo "patching done."
