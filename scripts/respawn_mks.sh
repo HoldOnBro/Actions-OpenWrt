32468af984d7db5a2a93ce75a8ca0ea2d5ccd294
HoldOnBro <357835338@qq.com>
2021-06-12 17:24:40 +0800

    pack one more firmware for each ARMv8 devices

--- scripts/respawn_mks.sh
+++ scripts/respawn_mks.sh
@@ -3,22 +3,33 @@
 cd openwrt
 sed -i '10,15 s/\(#\)\(.*\)/\2/' make.env
 OLD=$(grep \+o\" make.env)
-NEW=$(grep \+\" make.env)
+NEWP=$(grep 5\.10*\+\" make.env)
+NEWPP=$(grep 5\.12*\+\" make.env)
 #echo $OLD
 #echo $NEW
 cp make.env makeplus.env
-sed -i "s/$NEW/#$NEW/" make.env
+cp make.env makeplusplus.env
+sed -i "s/$NEWP/#$NEWP/" make.env
+sed -i "s/$NEWPP/#$NEWPP/" make.env
 sed -i "s/$OLD/#$OLD/" makeplus.env
+sed -i "s/$NEWPP/#$NEWPP/" makeplus.env
+sed -i "s/$OLD/#$OLD/" makeplusplus.env
+sed -i "s/$NEWP/#$NEWP/" makeplusplus.env
+
 #sync the kernel version
 KV=$(find /opt/kernel/ -name "boot*+o.tar.gz" | awk -F '[-.]' '{print $2"."$3"."$4"-"$5"-"$6}')
-KPV=$(find /opt/kernel/ -name "boot*+.tar.gz" | awk -F '[-.]' '{print $2"."$3"."$4"-"$5"-"$6}')
+KPV=$(find /opt/kernel/ -name "boot*5\.10*+.tar.gz" | awk -F '[-.]' '{print $2"."$3"."$4"-"$5"-"$6}')
+KPPV=$(find /opt/kernel/ -name "boot*5\.12*+.tar.gz" | awk -F '[-.]' '{print $2"."$3"."$4"-"$5"-"$6}')
 sed -i "s/^KERNEL_VERSION.*/KERNEL_VERSION=\"$KV\"/" make.env
 sed -i "s/^KERNEL_VERSION.*/KERNEL_VERSION=\"$KPV\"/" makeplus.env
+sed -i "s/^KERNEL_VERSION.*/KERNEL_VERSION=\"$KPPV\"/" makeplusplus.env
 
-for F in *.sh ; do cp $F ${F%.sh}_plus.sh && cp $F ${F%.sh}_fol.sh;done
+for F in *.sh ; do cp $F ${F%.sh}_fol.sh && cp $F ${F%.sh}_plus.sh && cp $F ${F%.sh}_plusplus.sh;done
 find ./* -maxdepth 1 -path "*_plus.sh" | xargs -i sed -i 's/make\.env/makeplus\.env/g' {}
-find ./*_plus.sh ./*_fol.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"F-openwrt/g' {}
-find ./*_plus.sh ./*_fol.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/F-openwrt/g' {}
+find ./* -maxdepth 1 -path "*_plusplus.sh" | xargs -i sed -i 's/make\.env/makeplusplus\.env/g' {}
+find ./*_fol.sh ./*_plus.sh ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"F-openwrt/g' {}
+find ./*_fol.sh ./*_plus.sh ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/F-openwrt/g' {}
+
 echo "mk_files respawned."
 
 cd ..
