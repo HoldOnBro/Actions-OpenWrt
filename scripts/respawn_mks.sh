469712ba010f1cc719f35a1c7d470b5d639209da
HoldOnBro <357835338@qq.com>
2021-07-02 00:23:19 +0800

    update
    Former-commit-id: 4bd7246ac8e165928ab7130cbd9e36f471592f03 [formerly 4bd7246ac8e165928ab7130cbd9e36f471592f03 [formerly fe0e23cb4a3cb5e3c85edff90b436ef38ec939fd [formerly 9126210d91b5f7b756bdf6f0db17844c8cb0a636]]]
    Former-commit-id: 79ca29835257f885ac667b9e19e6204df61ce371
    Former-commit-id: 1e8e3042ab1d1fe3eed2f6160307d9b858cf9067
    Former-commit-id: ace55f59c06dba64a56bd664b9b48b692f25cb0b

--- scripts/respawn_mks.sh
+++ scripts/respawn_mks.sh
@@ -24,15 +24,20 @@ sed -i "s/^KERNEL_VERSION.*/KERNEL_VERSION=\"$KV\"/" make.env
 sed -i "s/^KERNEL_VERSION.*/KERNEL_VERSION=\"$KPV\"/" makeplus.env
 sed -i "s/^KERNEL_VERSION.*/KERNEL_VERSION=\"$KPPV\"/" makeplusplus.env
 
-for F in *.sh ; do cp $F ${F%.sh}_sf.sh && cp $F ${F%.sh}_plus.sh && cp $F ${F%.sh}_plusplus.sh;done
+for F in *.sh ; do cp $F ${F%.sh}_basic.sh && cp $F ${F%.sh}_plus.sh && cp $F ${F%.sh}_plusplus.sh;done
 find ./* -maxdepth 1 -path "*_plus.sh" | xargs -i sed -i 's/make\.env/makeplus\.env/g' {}
 find ./* -maxdepth 1 -path "*_plusplus.sh" | xargs -i sed -i 's/make\.env/makeplusplus\.env/g' {}
+#统一用FOL打包
+find ./*_basic.sh ./*_plus.sh ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"F-openwrt/g' {}
+find ./*_basic.sh ./*_plus.sh ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/F-openwrt/g' {}
+
+#旧内核新内核分开打包
 #12以后内核无法使用SFE，使用仅含FOL的固件进行打包
-find ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"F-openwrt/g' {}
-find ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/F-openwrt/g' {}
+#find ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"F-openwrt/g' {}
+#find ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/F-openwrt/g' {}
 #旧内核支持SFE，使用含SFE和FOL的固件进行打包
-find ./*_sf.sh ./*_plus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"S-openwrt/g' {}
-find ./*_sf.sh ./*_plus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/S-openwrt/g' {}
+#find ./*_basic.sh ./*_plus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"S-openwrt/g' {}
+#find ./*_basic.sh ./*_plus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT_IMG=\"\${WORK_DIR}\/openwrt/TGT_IMG=\"\${WORK_DIR}\/S-openwrt/g' {}
 
 echo "mk_files respawned."
 
