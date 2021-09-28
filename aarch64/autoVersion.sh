4a45a9593fc638f554fa6a716410715f4c3f3e3b
HoldOnBro <357835338@qq.com>
2021-09-28 13:23:30 +0800

    update
    Former-commit-id: 6bff66fd354d7030023d9b89b1d24a97d6349b1d
    Former-commit-id: efcc7e69760a47fdbac58cd253b43bdad3b1f5dc
    Former-commit-id: 0fc73106a0d9e9e0d50c151e8e134dd2aa2ab980
    Former-commit-id: c21dbeb43b7a0f056013e10989e5cf2c9f28a89c

--- aarch64/autoVersion.sh
+++ aarch64/autoVersion.sh
@@ -3,6 +3,7 @@
 OpVersion=$(echo "$(cat /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings)" | grep -Po "DISTRIB_REVISION=\'\K[^\']*")
 
 echo $OpVersion
+echo $PWD
 
 sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /workdir/scripts/whoami
 #sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" `grep OPENWRT_VER=.* -rl /opt/openwrt/`
