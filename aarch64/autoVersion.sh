30cd4a4fadc08b345538b2df44d113a78f714dc2
HoldOnBro <357835338@qq.com>
2021-09-28 13:31:20 +0800

    Update autoVersion.sh
    Former-commit-id: a07ea55fef4f6a45986b370beb1b0ae8b5c43d47
    Former-commit-id: d49d909fc89157be82103b82ec2928cf589fe0c5
    Former-commit-id: 655b7088b7f281c0796bf0d2db5f13fafa6d5960
    Former-commit-id: 0ca4fc414e6fecd4223b9e68b7ea5925731abb5e

--- aarch64/autoVersion.sh
+++ aarch64/autoVersion.sh
@@ -5,7 +5,7 @@ OpVersion=$(echo "$(cat /workdir/openwrt/package/lean/default-settings/files/zzz
 echo $OpVersion
 echo $PWD
 
-sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /workdir/scripts/whoami
+sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /home/runner/work/Actions-OpenWrt/Actions-OpenWrt/scripts/whoami
 #sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" `grep OPENWRT_VER=.* -rl /opt/openwrt/`
 #sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_rk3328_beikeyun.sh
 #sed -i "s/OPENWRT_VER=.*/OPENWRT_VER=\"$OpVersion\"/" /opt/openwrt/mk_rk3328_beikeyun_tf.sh
