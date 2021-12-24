d0ae4eb5eedd1a951971c3fa3b0a94b6e30677ed
HoldOnBro <357835338@qq.com>
2021-12-24 09:03:57 +0800

    Update respawn_mks.sh
    Former-commit-id: 0a76de4cebd6b43189ea1cf9f8b82ada95c48c66 [formerly 0287ee781a3829ebe79142d3ef212d0745553c28]
    Former-commit-id: e345fea3f6aa3abc3fb2c1872072205ebd445b9b

--- scripts/respawn_mks.sh
+++ scripts/respawn_mks.sh
@@ -12,6 +12,8 @@ sed -i "s/^    KERNEL_VERSION.*/    KERNEL_VERSION=\"$KV\"/" make.env
 sed -i "s/^    KERNEL_VERSION.*/    KERNEL_VERSION=\"$KPV\"/" makeplus.env
 sed -i "s/^    KERNEL_VERSION.*/    KERNEL_VERSION=\"$KPPV\"/" makeplusplus.env
 
+cat make.env
+
 for F in *.sh ; do cp $F ${F%.sh}_basic.sh && cp $F ${F%.sh}_plus.sh && cp $F ${F%.sh}_plusplus.sh;done
 find ./* -maxdepth 1 -path "*_plus.sh" | xargs -i sed -i 's/make\.env/makeplus\.env/g' {}
 find ./* -maxdepth 1 -path "*_plusplus.sh" | xargs -i sed -i 's/make\.env/makeplusplus\.env/g' {}
