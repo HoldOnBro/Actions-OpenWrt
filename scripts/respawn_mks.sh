a6ccb56d09859e8a0cf54bbf0000735140317e12
HoldOnBro <357835338@qq.com>
2022-02-26 18:25:45 +0800

    SFE now working on all kernels
    Former-commit-id: f902935c8d0de3216f718f1cad4ac229ed4099e5
    Former-commit-id: 41aa19d533c853e7982667f94ae0f430f380168e

--- scripts/respawn_mks.sh
+++ scripts/respawn_mks.sh
@@ -21,11 +21,11 @@ find ./* -maxdepth 1 -path "*_plusplus.sh" | xargs -i sed -i 's/make\.env/makepl
 
 #旧内核新内核分开打包
 #12以后内核无法使用SFE，使用仅含FOL的固件进行打包
-find ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"F-openwrt/g' {}
-find ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i '/^TGT_IMG.*img\"$/s/\.img/\_FOL\.img/g' {}
-#旧内核支持SFE，使用含SFE和FOL的固件进行打包
-find ./*_basic.sh ./*_plus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"SF-openwrt/g' {}
-find ./*_basic.sh ./*_plus.sh -maxdepth 1 -path "*" | xargs -i sed -i '/^TGT_IMG.*img\"$/s/\.img/\_FOL\+SFE\.img/g' {}
+#find ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"F-openwrt/g' {}
+#find ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i '/^TGT_IMG.*img\"$/s/\.img/\_FOL\.img/g' {}
+#所有内核均已支持SFE，使用含SFE和FOL的固件进行打包
+find ./*_basic.sh ./*_plus.sh ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i 's/OP_ROOT_TGZ=\"openwrt/OP_ROOT_TGZ=\"SF-openwrt/g' {}
+find ./*_basic.sh ./*_plus.sh ./*_plusplus.sh -maxdepth 1 -path "*" | xargs -i sed -i '/^TGT_IMG.*img\"$/s/\.img/\_FOL\+SFE\.img/g' {}
 
 echo "mk_files respawned."
 
