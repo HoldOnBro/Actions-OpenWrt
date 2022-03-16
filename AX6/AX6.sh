0e6eab492158d374cb489134f6e83999ca3d16b7
HoldOnBro <357835338@qq.com>
2022-03-16 09:23:21 +0800

    remake AX6
    Former-commit-id: 91205da39d260a867276ccab8d08205af886e908
    Former-commit-id: 8d6869bf2149fc08da29bc2f84adf8b98f2584fa

--- AX6/AX6.sh
+++ AX6/AX6.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i "s|^root|root::0:0:99999:7:::|g" package/base-files/files/etc/shadow
