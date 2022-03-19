379c2497ccb5e208b40e8dd3a2b534101c446b0a
HoldOnBro <357835338@qq.com>
2022-03-19 09:43:42 +0800

    Update AX6.sh
    Former-commit-id: 763e01e993f09933d19a701fde45e450da93cadb
    Former-commit-id: 257fd53e3be1421e220f47f23a553d617e1108b8

--- AX6/AX6.sh
+++ AX6/AX6.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sed -i 's/10.10.10.1/192.168.31.1/g' package/base-files/files/bin/config_generate
-sed -i "s|^root|root::0:0:99999:7:::|g" package/base-files/files/etc/shadow
+sed -i "s|^root|root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::|g" package/base-files/files/etc/shadow
