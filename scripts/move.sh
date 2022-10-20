731c0488ce51a7cce74e8ca80092252a0090ec14
HoldOnBro <357835338@qq.com>
2022-10-20 10:11:54 +0800

    Update move.sh

--- scripts/move.sh
+++ scripts/move.sh
@@ -2,7 +2,7 @@
 
 cp -f /home/xyz/lede/ARMv8*.config /home/xyz/Actions-OpenWrt/aarch64/
 cp -f /home/xyz/lede/GDock.config /home/xyz/Actions-OpenWrt/GDock/
-cp -f /home/xyz/lede/AX6.config /home/xyz/Actions-OpenWrt/AX6/
+cp -f /home/xyz/lede-for-XaioMi/AX6.config /home/xyz/Actions-OpenWrt/AX6/
 cp -f /home/xyz/lede/J4125.config /home/xyz/Actions-OpenWrt/x86_64/
 cp -f /home/xyz/lede/x86_64.config /home/xyz/Actions-OpenWrt/x86_64/
 cp -f /home/xyz/lede/NewifiD2*.config /home/xyz/Actions-OpenWrt/NewifiD2/
