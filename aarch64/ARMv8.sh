e9ec396174b156ed1a4c79a5abfea44d2c6b73db
HoldOnBro <357835338@qq.com>
2022-12-14 08:38:53 +0800

    Update ARMv8.sh

--- aarch64/ARMv8.sh
+++ aarch64/ARMv8.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-sed -i 's/PKG_VERSION:=./PKG_VERSION:=4.8/g' tools/sed/Makefile
-sed -i 's/PKG_HASH:=./PKG_HASH:=f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633/g' tools/sed/Makefile
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=4.8/g' tools/sed/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633/g' tools/sed/Makefile
