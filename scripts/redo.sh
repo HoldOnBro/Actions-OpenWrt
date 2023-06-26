13cc792d8970fae6d9884ddd8f1bd0a75f32cda0
HoldOnBro <357835338@qq.com>
2023-06-26 13:26:51 +0800

    Update redo.sh

--- scripts/redo.sh
+++ scripts/redo.sh
@@ -90,20 +90,20 @@ cd /home/xyz/lede/package/luci-theme-opentopd && git pull
 cd /home/xyz/lede/package/luci-app-argon-config && git pull
 cd /home/xyz/lede/package/luci-theme-argon && git pull
 
-cd /home/xyz/luci-app-dockerman && git pull
-cd /home/xyz/OpenClash && git pull
-cd /home/xyz/immortalwrt-packages && git pull
-cd /home/xyz/immortalwrt-luci && git pull
-cd /home/xyz/openwrt-passwall && git pull
-cd /home/xyz/openwrt-passwall2 && git pull
-cd /home/xyz/helloworld && git pull
-cd /home/xyz/kenzok8-packages && git pull
-cd /home/xyz/luci-theme-darkmatter && git pull
-cd /home/xyz/luci-theme-opentomato && git pull
-cd /home/xyz/luci-theme-rosy && git pull
-cd /home/xyz/istore-ui && git pull
-cd /home/xyz/istore && git pull
-cd /home/xyz/luci-app-amlogic && git pull
+cd /home/xyz/lede/luci-app-dockerman && git pull
+cd /home/xyz/lede/OpenClash && git pull
+cd /home/xyz/lede/immortalwrt-packages && git pull
+cd /home/xyz/lede/immortalwrt-luci && git pull
+cd /home/xyz/lede/openwrt-passwall && git pull
+cd /home/xyz/lede/openwrt-passwall2 && git pull
+cd /home/xyz/lede/helloworld && git pull
+cd /home/xyz/lede/kenzok8-packages && git pull
+cd /home/xyz/lede/luci-theme-darkmatter && git pull
+cd /home/xyz/lede/luci-theme-opentomato && git pull
+cd /home/xyz/lede/luci-theme-rosy && git pull
+cd /home/xyz/lede/istore-ui && git pull
+cd /home/xyz/lede/istore && git pull
+cd /home/xyz/lede/luci-app-amlogic && git pull
 
 cp /home/xyz/Actions-OpenWrt/*/*.config /home/xyz/lede/
 
@@ -113,78 +113,78 @@ sed -i "s/\ \ \ \ path:.*/\ \ \ \ path:\ \"\*\"\,/" /home/xyz/sub-web/src/router
 yarn build
 cd /home/xyz/aclsub && git pull && yarn build
 
-cp -rf /home/xyz/luci-app-dockerman/applications/luci-app-dockerman /home/xyz/lede/package/luci-app-dockerman
-cp -rf /home/xyz/OpenClash/luci-app-openclash /home/xyz/lede/package/luci-app-openclash
+cp -rf /home/xyz/lede/luci-app-dockerman/applications/luci-app-dockerman /home/xyz/lede/package/luci-app-dockerman
+cp -rf /home/xyz/lede/OpenClash/luci-app-openclash /home/xyz/lede/package/luci-app-openclash
 
-cp -rf /home/xyz/immortalwrt-packages/net/brook /home/xyz/lede/package/brook
-cp -rf /home/xyz/immortalwrt-packages/net/gost /home/xyz/lede/package/gost
-cp -rf /home/xyz/immortalwrt-packages/libs/quickjspp /home/xyz/lede/package/quickjspp
-cp -rf /home/xyz/immortalwrt-packages/libs/jpcre2 /home/xyz/lede/package/jpcre2
-cp -rf /home/xyz/immortalwrt-packages/libs/libcron /home/xyz/lede/package/libcron
-cp -rf /home/xyz/immortalwrt-packages/libs/rapidjson /home/xyz/lede/package/rapidjson
-cp -rf /home/xyz/immortalwrt-packages/net/subconverter /home/xyz/lede/package/subconverter
-cp -rf /home/xyz/immortalwrt-packages/libs/toml11 /home/xyz/lede/package/toml11
-cp -rf /home/xyz/immortalwrt-packages/net/udp2raw /home/xyz/lede/package/udp2raw
+cp -rf /home/xyz/lede/immortalwrt-packages/net/brook /home/xyz/lede/package/brook
+cp -rf /home/xyz/lede/immortalwrt-packages/net/gost /home/xyz/lede/package/gost
+cp -rf /home/xyz/lede/immortalwrt-packages/libs/quickjspp /home/xyz/lede/package/quickjspp
+cp -rf /home/xyz/lede/immortalwrt-packages/libs/jpcre2 /home/xyz/lede/package/jpcre2
+cp -rf /home/xyz/lede/immortalwrt-packages/libs/libcron /home/xyz/lede/package/libcron
+cp -rf /home/xyz/lede/immortalwrt-packages/libs/rapidjson /home/xyz/lede/package/rapidjson
+cp -rf /home/xyz/lede/immortalwrt-packages/net/subconverter /home/xyz/lede/package/subconverter
+cp -rf /home/xyz/lede/immortalwrt-packages/libs/toml11 /home/xyz/lede/package/toml11
+cp -rf /home/xyz/lede/immortalwrt-packages/net/udp2raw /home/xyz/lede/package/udp2raw
 
-cp -rf /home/xyz/immortalwrt-luci/applications/luci-app-socat /home/xyz/lede/package/luci-app-socat
-cp -rf /home/xyz/immortalwrt-luci/applications/luci-app-gost /home/xyz/lede/package/luci-app-gost
-cp -rf /home/xyz/immortalwrt-luci/applications/luci-app-smartdns /home/xyz/lede/package/luci-app-smartdns
-cp -rf /home/xyz/immortalwrt-luci/applications/luci-app-udp2raw /home/xyz/lede/package/luci-app-udp2raw
+cp -rf /home/xyz/lede/immortalwrt-luci/applications/luci-app-socat /home/xyz/lede/package/luci-app-socat
+cp -rf /home/xyz/lede/immortalwrt-luci/applications/luci-app-gost /home/xyz/lede/package/luci-app-gost
+cp -rf /home/xyz/lede/immortalwrt-luci/applications/luci-app-smartdns /home/xyz/lede/package/luci-app-smartdns
+cp -rf /home/xyz/lede/immortalwrt-luci/applications/luci-app-udp2raw /home/xyz/lede/package/luci-app-udp2raw
 
-cp -rf /home/xyz/openwrt-passwall/chinadns-ng /home/xyz/lede/package/chinadns-ng
-cp -rf /home/xyz/openwrt-passwall/tcping /home/xyz/lede/package/tcping
-cp -rf /home/xyz/openwrt-passwall/trojan-go /home/xyz/lede/package/trojan-go
-cp -rf /home/xyz/openwrt-passwall/trojan-plus /home/xyz/lede/package/trojan-plus
-cp -rf /home/xyz/openwrt-passwall/ssocks /home/xyz/lede/package/ssocks
-cp -rf /home/xyz/openwrt-passwall/hysteria /home/xyz/lede/package/hysteria
-cp -rf /home/xyz/openwrt-passwall/dns2tcp /home/xyz/lede/package/dns2tcp
+cp -rf /home/xyz/lede/openwrt-passwall/chinadns-ng /home/xyz/lede/package/chinadns-ng
+cp -rf /home/xyz/lede/openwrt-passwall/tcping /home/xyz/lede/package/tcping
+cp -rf /home/xyz/lede/openwrt-passwall/trojan-go /home/xyz/lede/package/trojan-go
+cp -rf /home/xyz/lede/openwrt-passwall/trojan-plus /home/xyz/lede/package/trojan-plus
+cp -rf /home/xyz/lede/openwrt-passwall/ssocks /home/xyz/lede/package/ssocks
+cp -rf /home/xyz/lede/openwrt-passwall/hysteria /home/xyz/lede/package/hysteria
+cp -rf /home/xyz/lede/openwrt-passwall/dns2tcp /home/xyz/lede/package/dns2tcp
 
-cp -rf /home/xyz/openwrt-passwall2/luci-app-passwall2 /home/xyz/lede/package/luci-app-passwall2
+cp -rf /home/xyz/lede/openwrt-passwall2/luci-app-passwall2 /home/xyz/lede/package/luci-app-passwall2
 
-cp -rf /home/xyz/helloworld/naiveproxy /home/xyz/lede/package/naiveproxy
-cp -rf /home/xyz/helloworld/lua-neturl /home/xyz/lede/package/lua-neturl
-cp -rf /home/xyz/helloworld/gn /home/xyz/lede/package/gn
-cp -rf /home/xyz/helloworld/xray-core /home/xyz/lede/package/xray-core
-cp -rf /home/xyz/helloworld/xray-plugin /home/xyz/lede/package/xray-plugin
-cp -rf /home/xyz/helloworld/shadowsocks-rust /home/xyz/lede/package/shadowsocks-rust
-cp -rf /home/xyz/helloworld/shadowsocksr-libev /home/xyz/lede/package/shadowsocksr-libev
-cp -rf /home/xyz/helloworld/v2ray-plugin /home/xyz/lede/package/v2ray-plugin
-cp -rf /home/xyz/helloworld/simple-obfs /home/xyz/lede/package/simple-obfs
-cp -rf /home/xyz/helloworld/trojan /home/xyz/lede/package/trojan
-cp -rf /home/xyz/helloworld/v2ray-core /home/xyz/lede/package/v2ray-core
-cp -rf /home/xyz/helloworld/v2ray-geodata /home/xyz/lede/package/v2ray-geodata
-cp -rf /home/xyz/helloworld/sagernet-core /home/xyz/lede/package/sagernet-core
-cp -rf /home/xyz/helloworld/microsocks /home/xyz/lede/package/microsocks
-cp -rf /home/xyz/helloworld/redsocks2 /home/xyz/lede/package/redsocks2
-cp -rf /home/xyz/helloworld/ipt2socks /home/xyz/lede/package/ipt2socks
-cp -rf /home/xyz/helloworld/dns2socks /home/xyz/lede/package/dns2socks
-cp -rf /home/xyz/helloworld/luci-app-ssr-plus /home/xyz/lede/package/luci-app-ssr-plus
+cp -rf /home/xyz/lede/helloworld/naiveproxy /home/xyz/lede/package/naiveproxy
+cp -rf /home/xyz/lede/helloworld/lua-neturl /home/xyz/lede/package/lua-neturl
+cp -rf /home/xyz/lede/helloworld/gn /home/xyz/lede/package/gn
+cp -rf /home/xyz/lede/helloworld/xray-core /home/xyz/lede/package/xray-core
+cp -rf /home/xyz/lede/helloworld/xray-plugin /home/xyz/lede/package/xray-plugin
+cp -rf /home/xyz/lede/helloworld/shadowsocks-rust /home/xyz/lede/package/shadowsocks-rust
+cp -rf /home/xyz/lede/helloworld/shadowsocksr-libev /home/xyz/lede/package/shadowsocksr-libev
+cp -rf /home/xyz/lede/helloworld/v2ray-plugin /home/xyz/lede/package/v2ray-plugin
+cp -rf /home/xyz/lede/helloworld/simple-obfs /home/xyz/lede/package/simple-obfs
+cp -rf /home/xyz/lede/helloworld/trojan /home/xyz/lede/package/trojan
+cp -rf /home/xyz/lede/helloworld/v2ray-core /home/xyz/lede/package/v2ray-core
+cp -rf /home/xyz/lede/helloworld/v2ray-geodata /home/xyz/lede/package/v2ray-geodata
+cp -rf /home/xyz/lede/helloworld/sagernet-core /home/xyz/lede/package/sagernet-core
+cp -rf /home/xyz/lede/helloworld/microsocks /home/xyz/lede/package/microsocks
+cp -rf /home/xyz/lede/helloworld/redsocks2 /home/xyz/lede/package/redsocks2
+cp -rf /home/xyz/lede/helloworld/ipt2socks /home/xyz/lede/package/ipt2socks
+cp -rf /home/xyz/lede/helloworld/dns2socks /home/xyz/lede/package/dns2socks
+cp -rf /home/xyz/lede/helloworld/luci-app-ssr-plus /home/xyz/lede/package/luci-app-ssr-plus
 
-cp -rf /home/xyz/kenzok8-packages/smartdns /home/xyz/lede/package/smartdns
-cp -rf /home/xyz/kenzok8-packages/luci-app-passwall /home/xyz/lede/package/luci-app-passwall
-cp -rf /home/xyz/kenzok8-packages/luci-app-eqos /home/xyz/lede/package/luci-app-eqos
-cp -rf /home/xyz/kenzok8-packages/mosdns /home/xyz/lede/package/mosdns
-cp -rf /home/xyz/kenzok8-packages/luci-app-mosdns /home/xyz/lede/package/luci-app-mosdns
-cp -rf /home/xyz/kenzok8-packages/luci-theme-edge /home/xyz/lede/package/luci-theme-edge
-cp -rf /home/xyz/kenzok8-packages/luci-lib-taskd /home/xyz/lede/package/luci-lib-taskd
-cp -rf /home/xyz/kenzok8-packages/taskd /home/xyz/lede/package/taskd
-cp -rf /home/xyz/kenzok8-packages/luci-lib-xterm /home/xyz/lede/package/luci-lib-xterm
-cp -rf /home/xyz/kenzok8-packages/alist /home/xyz/lede/package/alist
-cp -rf /home/xyz/kenzok8-packages/luci-app-alist /home/xyz/lede/package/luci-app-alist
-cp -rf /home/xyz/kenzok8-packages/luci-app-advanced /home/xyz/lede/package/luci-app-advanced
-cp -rf /home/xyz/kenzok8-packages/ddnsto /home/xyz/lede/package/ddnsto
-cp -rf /home/xyz/kenzok8-packages/luci-app-ddnsto /home/xyz/lede/package/luci-app-ddnsto
-cp -rf /home/xyz/kenzok8-packages/luci-app-ikoolproxy /home/xyz/lede/package/luci-app-ikoolproxy
+cp -rf /home/xyz/lede/kenzok8-packages/smartdns /home/xyz/lede/package/smartdns
+cp -rf /home/xyz/lede/kenzok8-packages/luci-app-passwall /home/xyz/lede/package/luci-app-passwall
+cp -rf /home/xyz/lede/kenzok8-packages/luci-app-eqos /home/xyz/lede/package/luci-app-eqos
+cp -rf /home/xyz/lede/kenzok8-packages/mosdns /home/xyz/lede/package/mosdns
+cp -rf /home/xyz/lede/kenzok8-packages/luci-app-mosdns /home/xyz/lede/package/luci-app-mosdns
+cp -rf /home/xyz/lede/kenzok8-packages/luci-theme-edge /home/xyz/lede/package/luci-theme-edge
+cp -rf /home/xyz/lede/kenzok8-packages/luci-lib-taskd /home/xyz/lede/package/luci-lib-taskd
+cp -rf /home/xyz/lede/kenzok8-packages/taskd /home/xyz/lede/package/taskd
+cp -rf /home/xyz/lede/kenzok8-packages/luci-lib-xterm /home/xyz/lede/package/luci-lib-xterm
+cp -rf /home/xyz/lede/kenzok8-packages/alist /home/xyz/lede/package/alist
+cp -rf /home/xyz/lede/kenzok8-packages/luci-app-alist /home/xyz/lede/package/luci-app-alist
+cp -rf /home/xyz/lede/kenzok8-packages/luci-app-advanced /home/xyz/lede/package/luci-app-advanced
+cp -rf /home/xyz/lede/kenzok8-packages/ddnsto /home/xyz/lede/package/ddnsto
+cp -rf /home/xyz/lede/kenzok8-packages/luci-app-ddnsto /home/xyz/lede/package/luci-app-ddnsto
+cp -rf /home/xyz/lede/kenzok8-packages/luci-app-ikoolproxy /home/xyz/lede/package/luci-app-ikoolproxy
 
-cp -rf /home/xyz/luci-theme-darkmatter/luci/themes/luci-theme-darkmatter /home/xyz/lede/package/luci-theme-darkmatter
-cp -rf /home/xyz/luci-theme-opentomato/luci/themes/luci-theme-opentomato /home/xyz/lede/package/luci-theme-opentomato
-cp -rf /home/xyz/luci-theme-rosy/luci-theme-rosy /home/xyz/lede/package/luci-theme-rosy
+cp -rf /home/xyz/lede/luci-theme-darkmatter/luci/themes/luci-theme-darkmatter /home/xyz/lede/package/luci-theme-darkmatter
+cp -rf /home/xyz/lede/luci-theme-opentomato/luci/themes/luci-theme-opentomato /home/xyz/lede/package/luci-theme-opentomato
+cp -rf /home/xyz/lede/luci-theme-rosy/luci-theme-rosy /home/xyz/lede/package/luci-theme-rosy
 
-cp -rf /home/xyz/istore-ui/app-store-ui /home/xyz/lede/package/app-store-ui
-cp -rf /home/xyz/istore/luci/luci-app-store /home/xyz/lede/package/luci-app-store
+cp -rf /home/xyz/lede/istore-ui/app-store-ui /home/xyz/lede/package/app-store-ui
+cp -rf /home/xyz/lede/istore/luci/luci-app-store /home/xyz/lede/package/luci-app-store
 sed -i 's/luci-lib-ipkg/luci-base/g' /home/xyz/lede/package/luci-app-store/Makefile
 
-cp -rf /home/xyz/luci-app-amlogic/luci-app-amlogic /home/xyz/lede/package/luci-app-amlogic
+cp -rf /home/xyz/lede/luci-app-amlogic/luci-app-amlogic /home/xyz/lede/package/luci-app-amlogic
 
 #修改晶晨宝盒默认配置
 # 1.Set the download repository of the OpenWrt files to your github.com
