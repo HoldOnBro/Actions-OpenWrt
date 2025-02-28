93cc70e9f9862a739d65d74039ac50dc31ed960f
HoldOnBro <357835338@qq.com>
2025-02-28 07:17:00 +0000

    readd shadowsocksr-libev

--- diy-part2.sh
+++ diy-part2.sh
@@ -88,7 +88,7 @@ cp -rf openwrt-passwall2/luci-app-passwall2 package/luci-app-passwall2
 #rm -rf openwrt-passwall2
 
 # 克隆 helloworld 仓库
-git clone --depth=1 -b main https://github.com/fw876/helloworld.git
+git clone --depth=1 -b master https://github.com/fw876/helloworld.git
 # 从克隆的仓库中复制需要的目录
 cp -rf helloworld/naiveproxy package/naiveproxy
 cp -rf helloworld/lua-neturl package/lua-neturl
